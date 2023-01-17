c0e6a33482325ebd4fabbc7bcb20a059a7c53d2f
Lens <Lensdorsainvil711@outlook.com>
2023-01-16 23:04:29 -0500

    Added opacity transition

--- fitness.js
+++ fitness.js
@@ -4,3 +4,18 @@ var nav = document.querySelector('nav');
 menu.addEventListener('click', function() {
     nav.classList.toggle('navbar');
 })
+
+var hidden = document.querySelectorAll('.hidden');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      } else {
+        entry.target.classList.remove("show");
+      }
+    });
+  });
+
+ hidden.forEach((el) => observer.observe(el));
+  
